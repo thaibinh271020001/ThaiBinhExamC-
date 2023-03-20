a03461288fca280f34c7d948dfcd2767d026ee69
thaibinh271020001 <107410463+thaibinh271020001@users.noreply.github.com>
2023-03-20 16:43:55 +0700

    Add files via upload

---
+++ HomeWork/Lab3/TuLam3.4/TuLam3.4/Employee.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TuLam3._4
+{
+    class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int YearOfBirth { get; set; }
+        public double SalaryLevel { get; set; }
+        public double BasicSalary { get; set; }
+
+        public double GetIncome()
+        {
+            return SalaryLevel * BasicSalary;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("ID: " + Id);
+            Console.WriteLine("Name: " + Name);
+            Console.WriteLine("YearOfBirth: " + YearOfBirth);
+            Console.WriteLine("BasicSalary: " + BasicSalary);
+            Console.WriteLine(GetIncome());
+        }
+    }
+}
---
+++ HomeWork/Lab3/TuLam3.6/TuLam3.6/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TuLam3._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
