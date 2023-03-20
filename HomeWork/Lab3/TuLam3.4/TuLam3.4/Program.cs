34e1ea7c8bb78cbdb4621d1d9b0e35980db986d7
thaibinh271020001 <107410463+thaibinh271020001@users.noreply.github.com>
2023-03-20 16:52:49 +0700

    Add files via upload

---
+++ HomeWork/Lab3/TuLam3.4/TuLam3.4/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TuLam3._4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Chuong trinh lop employ");
+
+            Employee em = new Employee();
+
+            em.Id = 1;
+            em.Name = "Binh";
+            em.YearOfBirth = 2001;
+            em.SalaryLevel = 2.5;
+            em.BasicSalary = 10000000.5;
+
+            em.Display();
+        }
+    }
+}
